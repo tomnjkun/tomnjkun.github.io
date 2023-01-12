134c1243777b68ca68f3dc379443bbe0647bfaee
TonDP <45123896+tomnjkun@users.noreply.github.com>
2023-01-12 23:45:39 +0700

    update

--- test.js
+++ test.js
@@ -2,46 +2,6 @@ const scanButton = document.getElementById('scan')
 const writeButton = document.getElementById('write')
 const content2 = document.getElementById('text-test')
 
-if (/Chrome\/(\d+\.\d+.\d+.\d+)/.test(navigator.userAgent)){
-  // Let's log a warning if the sample is not supposed to execute on this
-  // version of Chrome.
-  if (89 > parseInt(RegExp.$1)) {
-    ChromeSamples.setStatus('Warning! Keep in mind this sample has been tested with Chrome ' + 89 + '.');
-  }
-}
-
-var ChromeSamples = {
-  log: function() {
-    var line = Array.prototype.slice.call(arguments).map(function(argument) {
-      return typeof argument === 'string' ? argument : JSON.stringify(argument);
-    }).join(' ');
-
-    document.querySelector('#log').textContent += line + '\n';
-  },
-
-  clearLog: function() {
-    document.querySelector('#log').textContent = '';
-  },
-
-  setStatus: function(status) {
-    document.querySelector('#status').textContent = status;
-  },
-
-  setContent: function(newContent) {
-    var content = document.querySelector('#content');
-    while(content.hasChildNodes()) {
-      content.removeChild(content.lastChild);
-    }
-    content.appendChild(newContent);
-  }
-};
-
-log = ChromeSamples.log;
-
-if (!("NDEFReader" in window)){
-  ChromeSamples.setStatus("Web NFC is not available. Use Chrome on Android.");
-}
-
 scanButton.addEventListener("click", async () => {
     log("User clicked scan button");
   
