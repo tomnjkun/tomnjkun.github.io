59d4c94e1260dd7c0aa9f04a50ebc70ac62f77e9
TonDP <45123896+tomnjkun@users.noreply.github.com>
2023-01-12 17:10:39 +0700

    update

--- test.js
+++ test.js
@@ -2,6 +2,46 @@ const scanButton = document.getElementById('scan')
 const writeButton = document.getElementById('write')
 const content = document.getElementById('text-test')
 
+if (/Chrome\/(\d+\.\d+.\d+.\d+)/.test(navigator.userAgent)){
+  // Let's log a warning if the sample is not supposed to execute on this
+  // version of Chrome.
+  if (89 > parseInt(RegExp.$1)) {
+    ChromeSamples.setStatus('Warning! Keep in mind this sample has been tested with Chrome ' + 89 + '.');
+  }
+}
+
+log = ChromeSamples.log;
+
+if (!("NDEFReader" in window)){
+  ChromeSamples.setStatus("Web NFC is not available. Use Chrome on Android.");
+}
+
+var ChromeSamples = {
+  log: function() {
+    var line = Array.prototype.slice.call(arguments).map(function(argument) {
+      return typeof argument === 'string' ? argument : JSON.stringify(argument);
+    }).join(' ');
+
+    document.querySelector('#log').textContent += line + '\n';
+  },
+
+  clearLog: function() {
+    document.querySelector('#log').textContent = '';
+  },
+
+  setStatus: function(status) {
+    document.querySelector('#status').textContent = status;
+  },
+
+  setContent: function(newContent) {
+    var content = document.querySelector('#content');
+    while(content.hasChildNodes()) {
+      content.removeChild(content.lastChild);
+    }
+    content.appendChild(newContent);
+  }
+};
+
 scanButton.addEventListener("click", async () => {
     log("User clicked scan button");
   
@@ -30,18 +70,6 @@ scanButton.addEventListener("click", async () => {
       const ndef = new NDEFReader();
       await ndef.write(content);
       log("> Message written");
-    } catch (error) {
-      log("Argh! " + error);
-    }
-  });
-  
-  makeReadOnlyButton.addEventListener("click", async () => {
-    log("User clicked make read-only button");
-  
-    try {
-      const ndef = new NDEFReader();
-      await ndef.makeReadOnly();
-      log("> NFC tag has been made permanently read-only");
     } catch (error) {
       log("Argh! " + error);
     }
