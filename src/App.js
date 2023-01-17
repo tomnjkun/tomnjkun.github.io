49018d4e57b8a6e0d8ec681f1a69dfb92a287c37
TonDP <45123896+tomnjkun@users.noreply.github.com>
2023-01-17 18:33:17 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import React from 'react';
 import NfcManager, {NfcTech} from 'react-native-nfc-manager';
 import {View, Text, TouchableOpacity} from 'react-native';
+import { useState } from 'react';
 
 NfcManager.start();
 
 function App() {
+  const [log,setLog] = useState('yeah')
   async function readNdef() {
     try {
       // register for the NFC tag with NDEF in it
@@ -11,8 +14,10 @@ function App() {
       // the resolved tag object will contain `ndefMessage` property
       const tag = await NfcManager.getTag();
       console.warn('Tag found', tag);
+      setLog('Tag found')
     } catch (ex) {
       console.warn('Oops!', ex);
+      setLog('oops')
     } finally {
       // stop the nfc scanning
       NfcManager.cancelTechnologyRequest();
@@ -23,6 +28,7 @@ function App() {
     <View>
       <TouchableOpacity onPress={readNdef}>
         <Text>Scan a Tag</Text>
+        <p>{log}</p>
       </TouchableOpacity>
     </View>
   );
