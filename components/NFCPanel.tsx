f833f883ac3425561305718aa826a8cf535df834
TonDP <45123896+tomnjkun@users.noreply.github.com>
2023-01-20 15:33:28 +0700

    Update NFCPanel.tsx

--- components/NFCPanel.tsx
+++ components/NFCPanel.tsx
@@ -1,9 +1,16 @@
 import { Button, Grid  } from "@nextui-org/react";
 import { useEffect, useState } from "react";
+import { Input } from "@nextui-org/react";
 import VCard from "vcard-creator";
 
 export default function NFCPanel() {
     const [log,setLog] = useState<String>('');
+    const [name,setName] = useState<String>('');
+    const [title,setTitle] = useState<String>('');
+    const [phone,setPhone] = useState<String>('');
+    const [email,setEmail] = useState<String>('');
+    const [workAddress,setWorkAddress] = useState<String>('');
+    const [homeAddress,setHomeAddress] = useState<String>('');
     useEffect(()=>{
         if (/Chrome\/(\d+\.\d+.\d+.\d+)/.test(navigator.userAgent)){
             // Let's log a warning if the sample is not supposed to execute on this
@@ -50,7 +57,8 @@ export default function NFCPanel() {
         console.log('enter3')
         try {
             const ndef = new NDEFReader();
-            const vcard = "BEGIN:VCARD\nVERSION:3.0\nFN:John Doe\nTEL;TYPE=WORK,VOICE:(123) 456-7890\nEMAIL;TYPE=WORK:john.doe@example.com\nEND:VCARD"
+            const vcard = `BEGIN:VCARD\nVERSION:3.0\nFN:${name}\nTITLE:${title}\nTEL;TYPE=WORK,VOICE:${phone}\nEMAIL;TYPE=WORK:${email}\nADR;TYPE#WORK,PREF:;;${workAddress}\n
+            ADR;TYPE#HOME:;;${homeAddress}\nEND:VCARD`
             const vcardAsArrayBuffer = new TextEncoder().encode(vcard);
             
             await ndef.write({records: [{recordType:"mime", mediaType:"text/vcard", data:vcardAsArrayBuffer}]});
@@ -87,6 +95,24 @@ export default function NFCPanel() {
     return (
         <>
         <Grid.Container gap={2}>
+            <Grid>
+                <Input rounded bordered label="Name" placeholder="John Doe" color="primary" onChange={ e => setName(e.target.value)}/>
+            </Grid>
+            <Grid>
+                <Input rounded bordered label="Title" placeholder="John" color="primary" onChange={ e => setTitle(e.target.value)}/>
+            </Grid>
+            <Grid>
+                <Input rounded bordered label="Telephone" placeholder="000-000-0000" color="primary" onChange={ e => setPhone(e.target.value)}/>
+            </Grid>
+            <Grid>
+                <Input rounded bordered label="Email" placeholder="Default@gmail.com" color="primary" onChange={ e => setEmail(e.target.value)}/>
+            </Grid>
+            <Grid>
+                <Input rounded bordered label="Work address" placeholder="Default" color="primary" onChange={ e => setWorkAddress(e.target.value)}/>
+            </Grid>
+            <Grid>
+                <Input rounded bordered label="Home address" placeholder="Default" color="primary" onChange={ e => setHomeAddress(e.target.value)}/>
+            </Grid>
             <Grid>
                 <Button bordered color="secondary" onPress={scan}>
                     Scan
