011ed0c47769ac989e02dd2a6b16d0cdd7fc711d
TonDP <45123896+tomnjkun@users.noreply.github.com>
2023-01-25 13:32:18 +0700

    Update [id].tsx

--- pages/catalogs/[id].tsx
+++ pages/catalogs/[id].tsx
@@ -1,16 +1,10 @@
 import { SimpleLayout } from "../../components/layout/simpleLayout"
-import { Container } from "@nextui-org/react"
+import { Button, Container } from "@nextui-org/react"
 import { useEffect } from "react";
+import Link from "next/link";
 
 export default function CatalogIndex() {
-  
-    useEffect(()=>{
-      var vcardString = `BEGIN:VCARD\nVERSION:3.0\nN:Sripairojthikoon;Nutisa;;;\nFN:Nutisa Sripairojthikoon\nORG:Digital Picnic Co., Ltd.\nCOMPANY:Digital Picnic Co., Ltd.\nTITLE:Co-Founder & Head of Software Engineer\nTEL;CELL;TYPE=mobile,VOICE:+66846557894\nEND:VCARD`
-      var element = document.createElement('a');
-      element.setAttribute('href', "data:text/x-vcard;urlencoded," + vcardString);
-      element.setAttribute('download', 'test.vcf');
-      element.click();
-    },[])
+    const vcardString = `BEGIN:VCARD\nVERSION:3.0\nN:Sripairojthikoon;Nutisa;;;\nFN:Nutisa Sripairojthikoon\nORG:Digital Picnic Co., Ltd.\nCOMPANY:Digital Picnic Co., Ltd.\nTITLE:Co-Founder & Head of Software Engineer\nTEL;CELL;TYPE=mobile,VOICE:+66846557894\nEND:VCARD`
 
     //let blob = new Blob([vcardString],{type:'text/vcard'})
     //let file = new File([blob], `test.vcf`,{type: "text/vcard"});
@@ -21,6 +15,9 @@ export default function CatalogIndex() {
       <>
         <div>
             <h1>hello</h1>
+            <Link href={"data:text/x-vcard;urlencoded," + vcardString}>
+              <Button>OPEN</Button>
+            </Link>
         </div>
       </>
     )
