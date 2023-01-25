c6cb106f2c690581336ee1d02dd7af4c79ced4f3
TonDP <45123896+tomnjkun@users.noreply.github.com>
2023-01-25 14:00:18 +0700

    Update [id].tsx

--- pages/catalogs/[id].tsx
+++ pages/catalogs/[id].tsx
@@ -1,13 +1,15 @@
 import { SimpleLayout } from "../../components/layout/simpleLayout"
 import { Button, Container } from "@nextui-org/react"
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Link from "next/link";
 
 export default function CatalogIndex() {
     const vcardString = `BEGIN:VCARD\nVERSION:3.0\nN:Sripairojthikoon;Nutisa;;;\nFN:Nutisa Sripairojthikoon\nORG:Digital Picnic Co., Ltd.\nCOMPANY:Digital Picnic Co., Ltd.\nTITLE:Co-Founder & Head of Software Engineer\nTEL;CELL;TYPE=mobile,VOICE:+66846557894\nEND:VCARD`
-
-    //let blob = new Blob([vcardString],{type:'text/vcard'})
-    //let file = new File([blob], `test.vcf`,{type: "text/vcard"});
+    const [file,setFile] = useState<File>();
+    useEffect(()=>{
+      let blob = new Blob([vcardString],{type:'text/vcard'})
+      setFile(new File([blob], `test.vcf`,{type: "text/vcard"}));
+    },[])
     //var a = document.createElement('a');
     //a.download= file;
     
@@ -15,9 +17,9 @@ export default function CatalogIndex() {
       <>
         <div>
             <h1>hello</h1>
-            <Link href={"data:text/x-vcard;urlencoded," + vcardString}>
+            <a download={file}>
               <Button>OPEN</Button>
-            </Link>
+            </a>
         </div>
       </>
     )
