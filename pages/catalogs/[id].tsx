e5dcf9437cc1bff0b64f9a662d67713eb270bffd
Suppachai Booncharoen <45123896+tomnjkun@users.noreply.github.com>
2023-02-01 16:50:08 +0700

    Update [id].tsx

--- pages/catalogs/[id].tsx
+++ pages/catalogs/[id].tsx
@@ -5,21 +5,14 @@ import Link from "next/link";
 
 export default function CatalogIndex() {
     const vcardString = `BEGIN:VCARD\nVERSION:3.0\nN:Sripairojthikoon;Nutisa;;;\nFN:Nutisa Sripairojthikoon\nORG:Digital Picnic Co., Ltd.\nCOMPANY:Digital Picnic Co., Ltd.\nTITLE:Co-Founder & Head of Software Engineer\nTEL;CELL;TYPE=mobile,VOICE:+66846557894\nEND:VCARD`
-    const [file,setFile] = useState<File>();
-    useEffect(()=>{
-      let blob = new Blob([vcardString],{type:'text/vcard'})
-      setFile(new File([blob], `test.vcf`,{type: "text/vcard"}));
-    },[])
-    //var a = document.createElement('a');
-    //a.download= file;
     
     return (
       <>
         <div>
             <h1>VCF download</h1>
-            <a download={file}>
-              <Button>OPEN</Button>
-            </a>
+            <Link href={"data:text/x-vcard;urlencoded," + vcardString}>
+               <Button>OPEN</Button>
+            </Link>
         </div>
       </>
     )
